cd7f70c4285365e40bb38eef89f63362a8e9be88
Dijets <39377778+lasthyphen@users.noreply.github.com>
2022-06-23 19:33:39 +0100

    Update versions.sh

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -7,4 +7,4 @@
 
 # Set up the versions to be used
 # Don't export them as their used in the context of other calls
-dijeth_version=${DIJETH_VERSION:-'v1.12.0'}
+dijeth_version=${DIJETH_VERSION:-'v2.4.2'}
