103a9c170e7e7152a168ca1acc48f31c7f61246f
Ubuntu <zehri@chains2.zbbh1gjdea1ubogdrkvir2n4ug.zrhx.internal.cloudapp.net>
2022-06-25 16:37:00 +0000

    genesis update

--- genesis/genesis_mainnet.go
+++ genesis/genesis_mainnet.go
@@ -15,51 +15,84 @@ var (
 		"networkID": 1,
 		"allocations": [
 			{
-				"ethAddr": "0x1B00f59fff05F6591c13e32740377eAE72661061",
-				"djtxAddr": "X-dijets1uxkc262hvgsvstguvawwaanmsh8zvtcsd64l5e",
-				"initialAmount": 2400000000000000,
+				"ethAddr": "0xd86b355443158939c2f1b2A00961F8453b33E74E",
+				"djtxAddr": "X-dijets1v8wat5z4cxh7wh873d7n6d9m6mpnynr8sgl059",
+				"initialAmount": 9285714000000000,
 				"unlockSchedule": [
 					{
-						"amount": 360000000000,
-						"locktime": 1646352000
+						"amount": 500000000000000,
+						"locktime": 1668115272
 					}
 				]
 			},
 			{
-				"ethAddr": "0xd86b355443158939c2f1b2A00961F8453b33E74E",
-				"djtxAddr": "X-dijets1v8wat5z4cxh7wh873d7n6d9m6mpnynr8sgl059",
-				"initialAmount": 24000000000000000,
+				"ethAddr": "0x1B00f59fff05F6591c13e32740377eAE72661061",
+				"djtxAddr": "X-dijets1uxkc262hvgsvstguvawwaanmsh8zvtcsd64l5e",
+				"initialAmount": 9285714000000000,
 				"unlockSchedule": [
 					{
-						"amount": 360000000000,
-						"locktime": 1646352000
+						"amount": 500000000000000,
+						"locktime": 1674115272
 					}
 				]
 			},
 			{
 				"ethAddr": "0x80231567cD6E270c8360B80b97034Ec26dad83b8",
 				"djtxAddr": "X-dijets16yd4ams4xdp9c6ht9zfnp90225ukwmqnj964sw",
-				"initialAmount": 2400000000000000,
+				"initialAmount": 9285714000000000,
 				"unlockSchedule": [
 					{
-						"amount": 360000000000,
-						"locktime": 1646352000
+						"amount": 500000000000000,
+						"locktime": 1676115272
 					}
 				]
 			},
 			{
-				"ethAddr": "0x2cf25c0323f52bb8d3f46ebc2dfcd34ba057b781",
+				"ethAddr": "0x2CF25c0323F52bb8d3f46EBc2DFcd34Ba057b781",
 				"djtxAddr": "X-dijets1mc6ar37ggvvh80ezkwnnyrkfummc32k0z9v9la",
-				"initialAmount": 240000000000,
+				"initialAmount": 9285714000000000,
 				"unlockSchedule": [
 					{
-						"amount": 360000000000,
-						"locktime": 1646352000
+						"amount": 500000000000000,
+						"locktime": 1688115272
+					}
+				]
+			},
+			{
+				"ethAddr": "0x1a8398064cbee749c62cc2943da234f75350390e",
+				"djtxAddr": "X-dijets1u3c5j2f3kg38eg6ryflljj3n7ark48gtr7s6dh",
+				"initialAmount": 9285714000000000,
+				"unlockSchedule": [
+					{
+						"amount": 500000000000000,
+						"locktime": 1688115272
+					}
+				]
+			},
+			{
+				"ethAddr": "0x62D4B8D6bdc42D4BB32B082c43fAdA8fF12E7130",
+				"djtxAddr": "X-dijets1vw0kjghl4nz3g9v5hsgn3l0xvgqetm678gcv09",
+				"initialAmount": 9285714000000000,
+				"unlockSchedule": [
+					{
+						"amount": 500000000000000,
+						"locktime": 1698115272
+					}
+				]
+			},
+			{
+				"ethAddr": "0xA0f60016666800936c534aB78cF3D9656654fAdE",
+				"djtxAddr": "X-dijets1seyylc9v8w9x5l6dpmf3se66hr37c34xnmd0r5",
+				"initialAmount": 9285714000000000,
+				"unlockSchedule": [
+					{
+						"amount": 500000000000000,
+						"locktime": 1698115272
 					}
 				]
 			}
 		],
-		"startTime": 1599696000,
+		"startTime": 1655861006,
 		"initialStakeDuration": 31536000,
 		"initialStakeDurationOffset": 54000,
 		"initialStakedFunds": [
@@ -92,8 +125,8 @@ var (
 				"delegationFee": 62500
 			}
 		],
-		"cChainGenesis": "{\"config\":{\"chainId\":98200,\"homesteadBlock\":0,\"daoForkBlock\":0,\"daoForkSupport\":true,\"eip150Block\":0,\"eip150Hash\":\"0x2086799aeebeae135c246c65021c82b4e15a2c451340993aacfd2751886514f0\",\"eip155Block\":0,\"eip158Block\":0,\"byzantiumBlock\":0,\"constantinopleBlock\":0,\"petersburgBlock\":0,\"istanbulBlock\":0,\"muirGlacierBlock\":0},\"nonce\":\"0x0\",\"timestamp\":\"0x0\",\"extraData\":\"0x00\",\"gasLimit\":\"0x5f5e100\",\"difficulty\":\"0x0\",\"mixHash\":\"0x0000000000000000000000000000000000000000000000000000000000000000\",\"coinbase\":\"0x0000000000000000000000000000000000000000\",\"alloc\":{\"0100000000000000000000000000000000000000\":{\"code\":\"0x7300000000000000000000000000000000000000003014608060405260043610603d5760003560e01c80631e010439146042578063b6510bb314606e575b600080fd5b605c60048036036020811015605657600080fd5b503560b1565b60408051918252519081900360200190f35b818015607957600080fd5b5060af60048036036080811015608e57600080fd5b506001600160a01b03813516906020810135906040810135906060013560b6565b005b30cd90565b836001600160a01b031681836108fc8690811502906040516000604051808303818888878c8acf9550505050505015801560f4573d6000803e3d6000fd5b505050505056fea26469706673582212201eebce970fe3f5cb96bf8ac6ba5f5c133fc2908ae3dcd51082cfee8f583429d064736f6c634300060a0033\",\"balance\":\"0x0\"}},\"number\":\"0x0\",\"gasUsed\":\"0x0\",\"parentHash\":\"0x0000000000000000000000000000000000000000000000000000000000000000\"}",
-		"message": "Pen Xen Ole Fren"
+		"cChainGenesis": "{\"config\":{\"chainId\":98200,\"homesteadBlock\":0,\"daoForkBlock\":0,\"daoForkSupport\":true,\"eip150Block\":0,\"eip150Hash\":\"0x2086799aeebeae135c246c65021c82b4e15a2c451340993aacfd2751886514f0\",\"eip155Block\":0,\"eip158Block\":0,\"byzantiumBlock\":0,\"constantinopleBlock\":0,\"petersburgBlock\":0,\"istanbulBlock\":0,\"muirGlacierBlock\":0,\"apricotPhase1BlockTimestamp\":0,\"apricotPhase2BlockTimestamp\":0,\"apricotPhase3BlockTimestamp\":0},\"nonce\":\"0x0\",\"timestamp\":\"0x0\",\"extraData\":\"0x00\",\"gasLimit\":\"0x5f5e100\",\"difficulty\":\"0x0\",\"mixHash\":\"0x0000000000000000000000000000000000000000000000000000000000000000\",\"coinbase\":\"0x0000000000000000000000000000000000000000\",\"alloc\":{\"8db97C7cEcE249c2b98bDC0226Cc4C2A57BF52FC\":{\"balance\":\"0x422CA8B0A00A425000000\"}},\"number\":\"0x0\",\"gasUsed\":\"0x0\",\"parentHash\":\"0x0000000000000000000000000000000000000000000000000000000000000000\"}",
+		"message": "{{ fun_quote }}"
 	}`
 
 	// MainnetParams are the params used for mainnet
